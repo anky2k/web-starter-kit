3ae6d1587ac9e6662b1ddd68a1ec31223566df20
anky2k <ankit.g.agarwal@gmail.com>
2021-09-01 12:04:32 +0530

    change all app icons

--- src/components/commons/head-meta/index.js
+++ src/components/commons/head-meta/index.js
@@ -29,7 +29,7 @@ function HeadMeta() {
       {/* Windows */}
       <meta name="msapplication-navbutton-color" content="red" />
       <meta name="msapplication-TileColor" content="red" />
-      <meta name="msapplication-TileImage" content={withBasePath('icons/favicon.ico')} />
+      <meta name="msapplication-TileImage" content={withBasePath('icons/favicon/favicon.ico')} />
       {/* Pinned Sites */}
       <meta name="msapplication-tooltip" content="Tooltip Text" />
       <meta name="msapplication-starturl" content="/" />
@@ -49,76 +49,76 @@ function HeadMeta() {
       <meta name="screen-orientation" content="portrait" />
 
       {/* Main Link Tags  */}
-      <link href={withBasePath('icons/favicon-16x16-dunlab-manifest-17016.png')} rel="icon" type="image/png" sizes="16x16" />
-      <link href={withBasePath('icons/favicon-32x32-dunlab-manifest-17016.png')} type="image/png" sizes="32x32" />
-      <link href={withBasePath('icons/favicon-96x96-dunlab-manifest-17016.png')} rel="icon" type="image/png" sizes="96x96" />
+      <link href={withBasePath('icons/favicon/favicon-16x16.png')} rel="icon" type="image/png" sizes="16x16" />
+      <link href={withBasePath('icons/favicon/favicon-32x32.png')} type="image/png" sizes="32x32" />
+      <link href={withBasePath('icons/favicon/favicon-96x96.png')} rel="icon" type="image/png" sizes="96x96" />
 
       {/* iOS  */}
-      <link href={withBasePath('icons/apple-touch-icon.png')} rel="apple-touch-icon" />
-      <link href={withBasePath('icons/apple-icon-76x76-dunlab-manifest.png')} rel="apple-touch-icon" sizes="76x76" />
-      <link href={withBasePath('icons/apple-icon-120x120-dunlab-manifest.png')} rel="apple-touch-icon" sizes="120x120" />
-      <link href={withBasePath('icons/apple-icon-152x152-dunlab-manifest.png')} rel="apple-touch-icon" sizes="152x152" />
-      <link href={withBasePath('icons/apple-icon-180x180-dunlab-manifest.png')} rel="apple-touch-icon" sizes="180x180" />
+      <link href={withBasePath('icons/ios/icon-72.png')} rel="apple-touch-icon" />
+      <link href={withBasePath('icons/ios/icon-76.png')} rel="apple-touch-icon" sizes="76x76" />
+      <link href={withBasePath('icons/ios/icon-120.png')} rel="apple-touch-icon" sizes="120x120" />
+      <link href={withBasePath('icons/ios/icon-152.png')} rel="apple-touch-icon" sizes="152x152" />
+      <link href={withBasePath('icons/ios/icon-180.png')} rel="apple-touch-icon" sizes="180x180" />
 
       {/* Startup Image  - splash screens for i phones */}
       <link
         rel="apple-touch-startup-image"
-        href={withBasePath('icons/apple-icon-76x76-dunlab-manifest.png')}
+        href={withBasePath('icons/ios/icon-76.png')}
         media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       />
 
       <link
         rel="apple-touch-startup-image"
-        href={withBasePath('icons/apple-icon-76x76-dunlab-manifest.png')}
+        href={withBasePath('icons/ios/icon-76.png')}
         media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
       />
 
       <link
         rel="apple-touch-startup-image"
-        href={withBasePath('icons/apple-icon-76x76-dunlab-manifest.png')}
+        href={withBasePath('icons/ios/icon-76.png')}
         media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       />
 
       <link
         rel="apple-touch-startup-image"
-        href={withBasePath('icons/apple-icon-76x76-dunlab-manifest.png')}
+        href={withBasePath('icons/ios/icon-76.png')}
         media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)"
       />
 
       <link
         rel="apple-touch-startup-image"
-        href={withBasePath('icons/apple-icon-76x76-dunlab-manifest.png')}
+        href={withBasePath('icons/ios/icon-76.png')}
         media="(min-device-width: 768px) and (max-device-width: 1024px)
         and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
       />
 
       <link
         rel="apple-touch-startup-image"
-        href={withBasePath('icons/apple-icon-76x76-dunlab-manifest.png')}
+        href={withBasePath('icons/ios/icon-76.png')}
         media="(min-device-width: 834px) and (max-device-width: 834px)
         and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
       />
 
       <link
         rel="apple-touch-startup-image"
-        href={withBasePath('icons/apple-icon-76x76-dunlab-manifest.png')}
+        href={withBasePath('icons/ios/icon-76.png')}
         media="(min-device-width: 1024px) and (max-device-width: 1024px)
         and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)"
       />
 
       {/* Pinned Tab  */}
-      <link href={withBasePath('icons/favicon.ico')} rel="mask-icon" size="any" color="red" />
+      <link href={withBasePath('icons/favicon/favicon.ico')} rel="mask-icon" size="any" color="red" />
 
       {/* Android  */}
-      <link href={withBasePath('icons/android-chrome-192x192.png')} rel="icon" sizes="192x192" />
-      <link href={withBasePath('icons/android-chrome-192x192.png')} rel="icon" sizes="128x128" />
+      <link href={withBasePath('icons/android/android-launchericon-192x192.png')} rel="icon" sizes="192x192" />
+      <link href={withBasePath('icons/android/android-launchericon-192x192.png')} rel="icon" sizes="128x128" />
 
       {/* Others */}
-      <link href={withBasePath('icons/favicon.icon')} rel="shortcut icon" type="image/x-icon" />
+      <link href={withBasePath('icons/favicon/favicon.icon')} rel="shortcut icon" type="image/x-icon" />
 
       {/* UC Browser  */}
 
-      <link rel="shortcut icon" href={withBasePath('/icons/favicon.ico')} />
+      <link rel="shortcut icon" href={withBasePath('/icons/favicon/favicon.ico')} />
 
       <link rel="manifest" href={withBasePath(`/manifest.json?v=${appVersion}`)} />
     </>
