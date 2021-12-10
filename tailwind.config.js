51dfad550f2167349cf8515697c976de6f8a7063
anky2k <ankit.agarwal@fox.com>
2021-12-10 11:33:49 +0530

    upgrade to tailwind 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
   future: {
     removeDeprecatedGapUtilities: true
   },
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   presets: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
@@ -117,7 +117,7 @@ module.exports = {
       },
       black: colors.black,
       white: colors.white,
-      gray: colors.coolGray,
+      gray: colors.gray,
       red: colors.red,
       yellow: colors.amber,
       green: colors.emerald,
